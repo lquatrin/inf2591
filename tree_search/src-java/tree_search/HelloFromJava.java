3e05979a1d143a23feb623e95bd0a431b14d61a4
Leonardo Quatrin Campagnolo <campagnolo.lq@gmail.com>
2016-11-30 19:25:37 -0200

    links

--- tree_search/src-java/tree_search/HelloFromJava.java
+++ tree_search/src-java/tree_search/HelloFromJava.java
@@ -1,3 +1,19 @@
+/*
+ * https://snap.stanford.edu/data/
+ * 
+ * http://www5.in.tum.de/lehre/praktika/ftcp/04_advanced.pdf
+ * http://jvde.us/forum/index.php?topic=6.0
+ * http://x10-lang.org/documentation/x10dt-installation.html
+ * http://forums.x10.com/index.php?topic=26297.0
+ * http://x10.sourceforge.net/documentation/languagespec/x10-latest.pdf
+ * https://www.cs.colostate.edu/wiki/mediawiki/images/5/5d/X10programmingguide.pdf
+ * http://x10.sourceforge.net/documentation/intro/latest/html/
+ * 
+ * 
+ * http://saraswat.org/graph.pdf
+ * http://x10-lang.org/articles/154.html
+ */
+
 package tree_search;
 
 import tree_search.RDFS;
