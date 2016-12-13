a19eb3c941c577628f24d6cedd545bc004a64f71
Leonardo Quatrin Campagnolo <campagnolo.lq@gmail.com>
2016-12-13 14:01:00 -0200

    última correção pra n dar erro no package

--- tree_search/src-java/tree_search/HelloFromJava.java
+++ tree_search/src-java/tree_search/HelloFromJava.java
@@ -16,7 +16,6 @@
 
 package tree_search;
 
-import tree_search.RDFS;
 
 //import x10.x10rt.X10RT;
 
@@ -24,13 +23,6 @@ public class HelloFromJava {
 
 	public static void main(String[] args) {
 		System.out.println("Hello From Java");
-		
-		RDFS r = new RDFS();
-		
-		System.out.println("x: " + r.x + ", y:" + r.y);
-		System.out.println("Magnitude: " + r.magnitude$O());
-		
-		r.prints();
 	}
 
 }
